b0fe16db749164cf0feb383cca0b67cc3d5c6837
CoderAbhi101 <coderabhi101@gmail.com>
2025-01-16 20:15:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,8 @@ txs = []
 
 @app.route("/")
 def home():
-    return render_template("home.html", txs=txs.reverse())
+    txs.reverse()
+    return render_template("home.html", txs=txs)
 
 def run():
     app.run(host="0.0.0.0", port=8080)
