358791fc2b2f1819339b627c1c8085b270d372a9
CoderAbhi101 <coderabhi101@gmail.com>
2025-01-16 20:34:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ txs = []
 
 @app.route("/")
 def home():
-    txs.reverse()
-    return render_template("home.html", txs=txs)
+    reversed_txs = list(reversed(txs))
+    return render_template("home.html", txs=reversed_txs)
 
 def run():
     app.run(host="0.0.0.0", port=8080)
@@ -18,15 +18,19 @@ t = Thread(target=run)
 t.start()
 
 def claim_faucet():
-    try:
-        response = requests.post("https://faucet.testnet.humanity.org/api/claim", json={"address": "0x01fdc84aa8074f74794E095AE9347b6538817050"})
-        if response.status_code == 200:
-            response2 = requests.get("https://timeapi.io/api/Time/current/zone?timeZone=Asia/Kolkata")
-            if response2.status_code == 200:
-                global txs
-                txs.append({'date': response2.json()["date"], 'time': response2.json()["time"], 'txhash': response.json()["msg"]})
-    except Exception as e:
-        print(f"Error occurred: {e}")
+    attempts = 0
+    while attempts < 100:
+        try:
+            attempts += 1
+            response = requests.post("https://faucet.testnet.humanity.org/api/claim", json={"address": "0x01fdc84aa8074f74794E095AE9347b6538817050"})
+            if response.status_code == 200:
+                response2 = requests.get("https://timeapi.io/api/Time/current/zone?timeZone=Asia/Kolkata")
+                if response2.status_code == 200:
+                    global txs
+                    txs.append({'date': response2.json()["date"], 'time': response2.json()["time"], 'txhash': response.json()["msg"]})
+                    break
+        except Exception as e:
+            print(f"Error occurred: {e}")
 
 while True:
     claim_faucet()
