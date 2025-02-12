0adeb92fd153f1f508ce93b54fc94cfe8328f61a
CoderAbhi101 <coderabhi101@gmail.com>
2025-02-13 01:26:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ def claim_faucet():
                 response2 = requests.get("https://timeapi.io/api/Time/current/zone?timeZone=Asia/Kolkata")
                 if response2.status_code == 200:
                     global txs
-                    txs.append({'date': response2.json()["date"], 'time': response2.json()["time"], 'txhash': response.json()["msg"]}, 'wallet': '0x01fdc84aa8074f74794E095AE9347b6538817050')
+                    txs.append({'date': response2.json()["date"], 'time': response2.json()["time"], 'txhash': response.json()["msg"], 'wallet': '0x01fdc84aa8074f74794E095AE9347b6538817050'})
                     break
         except Exception as e:
             print(f"Error occurred: {e}")
@@ -40,7 +40,7 @@ def claim_faucet():
                 response2 = requests.get("https://timeapi.io/api/Time/current/zone?timeZone=Asia/Kolkata")
                 if response2.status_code == 200:
                     global txs
-                    txs.append({'date': response2.json()["date"], 'time': response2.json()["time"], 'txhash': response.json()["msg"]}, 'wallet': '0x673fcF2440FDe7A154150bAb04853A4583bE9d69')
+                    txs.append({'date': response2.json()["date"], 'time': response2.json()["time"], 'txhash': response.json()["msg"], 'wallet': '0x673fcF2440FDe7A154150bAb04853A4583bE9d69'})
                     break
         except Exception as e:
             print(f"Error occurred: {e}")
